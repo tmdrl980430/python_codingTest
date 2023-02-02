7e1df5d48962c839f0948cf8d15536c6fce470bc
tmdrl980430 <tmdrl980430@naver.com>
2023-02-03 03:36:56 +0900

    1021번

--- 1021.py
+++ 1021.py
@@ -2,32 +2,50 @@ from collections import deque
 import sys
 
 n , m = map(int, sys.stdin.readline().split())
-arr_queue = deque()
+
+temp_queue1 = deque()
+temp_queue2 = deque()
+
+
 
 for i in range(1, n+1):
-    arr_queue.append(i)
+    temp_queue1.append(i)
+    temp_queue2.append(i)
 arr = list(map(int, sys.stdin.readline().split()))
 
 answer = 0
-# for i in range(len(arr)):
-#     for j in  range(len(arr_queue)):
+for i in range(len(arr)):
+    answer1 = 0
+    answer2 = 0
+    length = len(temp_queue1)
+    for z in range(2):
+        for j in  range(length):
+            if z == 0:
+                temp = temp_queue1.popleft()
+            else:
+                temp = temp_queue2[0]
+            if temp == arr[i]:
+                if z == 1:
+                    temp_queue2.popleft()
+                break
+            else:
+                if z == 0:
+                    answer1 += 1
+                    temp_queue1.append(temp)
+                else:
+                    answer2 += 1
+                    temp = temp_queue2.pop()
+                    temp_queue2.appendleft(temp)
+                continue
+            
         
-#         temp = arr_queue.popleft()
+    if  answer1 == answer2:
+        answer += answer1
+    elif answer1 < answer2:
+        answer += answer1
+    else:
+        answer += answer2
         
-#         if temp == arr[i]:
-#             break
-#         else:
-#             continue
-rightMove()
-print(arr_queue)
-leftMove()
-print(arr_queue)
 
-def rightMove():
-    tempRight = arr_queue.popleft()
-    arr_queue = arr_queue.append(tempRight)
 
-def leftMove ():
-    tempLeft = arr_queue.pop()
-    arr_queue = arr_queue.appendleft(tempLeft)
 print(answer)
