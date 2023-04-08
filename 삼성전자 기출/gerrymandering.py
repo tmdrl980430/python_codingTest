e522f0aad2238818c9c030458e6aab6ba80a4bc2
Duke(김승기) <74481440+tmdrl980430@users.noreply.github.com>
2023-04-08 17:23:04 +0900

    .

--- 삼성전자 기출/gerrymandering.py
+++ 삼성전자 기출/gerrymandering.py
@@ -1,13 +1,60 @@
+import sys
+from itertools import combinations
+from collections import deque
 
-# n = 구역의 개수
-n = int(input())
-
-#구역의 인구 리스트
-population_list = list(map(int, input().split()))
-
-place_dict = {}
+n = int(sys.stdin.readline().rstrip())
+people = list(map(int, sys.stdin.readline().rstrip().split()))
+arr = [[0 for _ in range(n)] for _ in range(n)]
 
 for i in range(n):
-    adjacency_place = list(map(int, input().split()))
-    place_dict[i + 1] = [adjacency_place]
-print(place_dict)
+    _, *dsts = map(int, sys.stdin.readline().rstrip().split())
+    for dst in dsts:
+        arr[i][dst - 1] = 1
+
+
+def is_connected(nodes):
+    q = deque()
+    check = [False for _ in range(n)]
+    q.append(nodes[0])
+    check[nodes[0]] = True
+
+    while q:
+        node = q.popleft()
+
+        for i in range(len(arr[node])):
+            if arr[node][i] == 0: continue
+            if i not in nodes: continue
+            if not check[i]:
+                check[i] = True
+                q.append(i)
+
+    return check.count(True) == len(nodes)
+
+
+def get_total(nodes):
+    total = 0
+    for node in nodes:
+        total += people[node]
+
+    return total
+
+
+cases = []
+X = {i for i in range(n)}
+INF = int(1e9)
+ans = INF
+
+for i in range(1, n // 2 + 1):
+    As = tuple(combinations(X, i))
+    for A in As:
+        B = list(X.difference(A))
+
+        if is_connected(A) and is_connected(B):
+            a_total = get_total(A)
+            b_total = get_total(B)
+            ans = min(ans, abs(a_total - b_total))
+
+if ans == INF:
+    print(-1)
+else:
+    print(ans)
