1b0da4899a43d18665a1afbfe9dc9def99e52f19
tmdrl980430 <tmdrl980430@naver.com>
2023-05-31 23:45:03 +0900

    13460

--- 13460.py
+++ 13460.py
@@ -1,100 +1,77 @@
-import sys
 from collections import deque
 
-n , m = map(int, sys.stdin.readline().split())
+# 이동 방향: 상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-matrix = [[] * m for _ in range(n)]
+# 구슬 이동 함수
+def move(x, y, dx, dy):
+    count = 0
+    # 다음 위치로 이동할 수 있을 때까지 이동
+    while board[x+dx][y+dy] != '#' and board[x][y] != 'O':
+        x += dx
+        y += dy
+        count += 1
+    return x, y, count
 
-blue_x = 0
-blue_y = 0
+# BFS 탐색 함수
+def bfs(rx, ry, bx, by):
+    queue = deque([(rx, ry, bx, by, 1)])
+    visited = set([(rx, ry, bx, by)])
+    
+    while queue:
+        rx, ry, bx, by, depth = queue.popleft()
+        
+        # 최대 10번 이동까지만 확인
+        if depth > 10:
+            break
+        
+        # 4가지 이동 방향에 대해
+        for i in range(4):
+            nrx, nry, rcnt = move(rx, ry, dx[i], dy[i])
+            nbx, nby, bcnt = move(bx, by, dx[i], dy[i])
+            
+            # 파란 구슬이 구멍에 빠졌을 경우는 실패이므로 다음 방향 확인
+            if board[nbx][nby] == 'O':
+                continue
+            
+            # 빨간 구슬이 구멍에 빠졌을 경우는 성공이므로 결과 반환
+            if board[nrx][nry] == 'O':
+                return depth
+            
+            # 빨간 구슬과 파란 구슬이 겹칠 경우 위치 조정
+            if nrx == nbx and nry == nby:
+                if rcnt > bcnt:
+                    nrx -= dx[i]
+                    nry -= dy[i]
+                else:
+                    nbx -= dx[i]
+                    nby -= dy[i]
+            
+            # 이전에 방문한 상태가 아니라면 큐에 추가
+            if (nrx, nry, nbx, nby) not in visited:
+                queue.append((nrx, nry, nbx, nby, depth + 1))
+                visited.add((nrx, nry, nbx, nby))
+    
+    # 실패일 경우 -1 반환
+    return -1
 
-red_x = 0
-red_y = 0
 
-hole_x = 0
-hole_y = 0
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+# 입력 처리
+n, m = map(int, input().split())
+board = []
+rx, ry, bx, by = 0, 0, 0, 0
 
 for i in range(n):
-    temp = sys.stdin.readline()
-    for j in range(m):
-        matrix[i].append(temp[j])
-        if temp[j] == "B":
-            blue_x = i
-            blue_y = j
-        elif temp[j] == "R":
-            red_x = i
-            red_y = j
-        elif temp[j] == "O":
-            hole_x = i
-            hole_y = j
+    row = list(input().strip())
+    board.append(row)
+    if 'R' in row:
+        rx, ry = i, row.index('R')
+    if 'B' in row:
+        bx, by = i, row.index('B')
 
-def bfs(a ,b , c, d):
-    queue = deque()
-    queue.append((a,b,c,d))
-    visited = [] # 방문여부를 판단하기 위한 리스트
-    visited.append((a, b, c, d))
-    count = 0
-    while queue:
-        for _ in range(len(queue)):
-            
-            rx , ry , bx, by = queue.popleft()
-            
-            if count > 10:
-                print(-1)
-                return
-            if matrix[rx][ry] == 'O':
-                print(count)
-                return
-            for i in range(4):
-                nrx, nry = rx, ry
-                while True:
-                    nrx += dx[i]
-                    nry += dy[i]
-                    if nrx < 0 or nry < 0 or nrx >= n or nry >= m:
-                        nrx -= dx[i]
-                        nry -= dy[i]
-                        break
-                    if matrix[nrx][nry] == "#":
-                        nrx -= dx[i]
-                        nry -= dy[i]
-                        break
-                    if matrix[nrx][nry] == 'O':
-                        break
-                nbx, nby = bx, by
-                while True:
-                    nbx += dx[i]
-                    nby += dy[i]
-                    if nbx < 0 or nby < 0 or nbx >= n or nby >= m:
-                        nbx -= dx[i]
-                        nby -= dy[i]
-                        break
-                    if matrix[nbx][nby] == "#":
-                        nrx -= dx[i]
-                        nry -= dy[i]
-                        break
-                    if matrix[nbx][nby] == 'O':
-                        break
-                if nbx < 0 or nby < 0 or nbx >= n or nby >= m or nrx < 0 or nry < 0 or nrx >= n or nry >= m:
-                    continue
-                elif matrix[nbx][nby] == 'O':
-                    continue
-                if nrx == nbx and nry == nby:
-                    if abs(nrx - rx) + abs(nry - ry) > abs(nbx - bx) + abs(nby - by):
-                        nrx -= dx[i]
-                        nry -= dy[i]
-                    else:
-                        nbx -= dx[i]
-                        nby -= dy[i]
-                if (nrx, nry, nbx, nby) not in visited:
-                    queue.append((nrx, nry, nbx, nby))
-                    visited.append(((nrx, nry, nbx, nby)))
-        count += 1
-    print(-1)
-bfs(red_x, red_y, blue_x, blue_y)
-                
-                
-                
-                
+# BFS 탐색 실행
+result = bfs(rx, ry, bx, by)
+
+# 결과 출력
+print(result)
