8062bb9a214e33a3bc20852b4a6e16983952786e
tmdrl980430 <tmdrl980430@naver.com>
2023-06-14 21:57:16 +0900

    정말 간단한 시뮬레이션...

--- 14503.py
+++ 14503.py
@@ -13,85 +13,46 @@
     # 3-2 바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한칸 전진한다.
     # 3-3 1번으로 돌아간다.
 import sys
+input = sys.stdin.readline
 from collections import deque
 
-input = sys.stdin.readline
-N , M = map(int, input().split())
+n,m = map(int,input().split())
+graph = []
+visited = [[0] * m for _ in range(n)]
+r,c,d = map(int,input().split())
 
-matrix = []
+# d => 0,3,2,1 순서로 돌아야한다.
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
 
-r, c, d = map(int, input().split())
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
 
-dx = [0 ,1, 0, -1]
-dy = [1, 0, -1 , 0]
-# 동 북 서 남
+# 처음 시작하는 곳 방문 처리
+visited[r][c] = 1
+cnt = 1
 
-visited = [[False] * N for _ in range(M)]
-
-for i in range(N):
-    temp =list(map(int, input().split()))
-    matrix.append(temp)
-
-answer = 0
-# 반시계 방향으로 회전한다 ===> 동 -> 북 -> 서 -> 남 -> 동 이런식으로 회전한다는 말
-
-def clear(x ,y):
-    global d, answer, visited, matrix
-    #현재 칸이 아직 청소되지 않은 경우 현재 칸을 청소한다.
-    if visited[x][y] == False and matrix[x][y] == 0:
-        visited[x][y] = True
-        answer += 1
-        return x, y
-    # 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우
-    for i in range(4): #주변 4칸에 청소되지 않은 빈 칸이 있는지 없는지 판별만 하기;
-        nx = dx[i] + x
-        ny = dy[i] + y
-        if nx < 0 or  ny < 0 or nx >= N or ny >= M:
-            continue
-        if matrix[nx][ny] == 0:
-            a, b = third_clear(x, y)# 주변 4칸중에 빈칸이면서 청소가 안되어 있으면 queue 삽입
-            if a == x and b == y:
-                continue 
-
-    a, b = second_clear(x,y)
-    if a == -1 and b == -1:
-        return -1,-1
-    
-    return x, y
-
-def second_clear(x,y):
-    global d
-    #바라보는 방향을 유지한 채로 한칸 후진할 수 있다면 후진하고 1번으로 돌아감
-    if matrix[x - dx[d]][y - dy[d]] == 0:
-        # 후진할 수 있다면
-        x = x - dx[d]
-        y = y - dy[d]
-        #후진하고
-        #1번으로 돌아감
-        return x, y
-    else:
-        return -1,-1
-
-
-
-def third_clear(x,y) :
-    global d
-    d += 1
-    if d  == 4:
-        d = 0
-    ax = x + dx[d]
-    ay = y + dy[d]
-    if ax < 0 or  ay < 0 or ax >= N or ay >= M:
-        return x , y
-    if matrix[ax][ay] == 0 and visited[ax][ay] == False:
-        return ax, ay #전진한 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진한다. 그리고 1번으로 돌아감
-    elif matrix[ax][ay] == 0 and visited[ax][ay] == True:
-        return x , y # 전진한 칸이 청소가 되어 있으면 그냥 1번으로 돌아감
-
-while True:
-    
-    answer1, answer2 = clear(r,c)
-    if answer1 == -1 and answer2 == -1:
-        break
-
-print(answer)
+while 1:
+    flag = 0
+    # 4방향 확인
+    for _ in range(4):
+        # 0,3,2,1 순서 만들어주기위한 식
+        nx = r + dx[(d+3)%4]
+        ny = c + dy[(d+3)%4]
+        # 한번 돌았으면 그 방향으로 작업시작
+        d = (d+3)%4
+        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+            if visited[nx][ny] == 0:
+                visited[nx][ny] = 1
+                cnt += 1
+                r = nx
+                c = ny
+                #청소 한 방향이라도 했으면 다음으로 넘어감
+                flag = 1
+                break
+    if flag == 0: # 4방향 모두 청소가 되어 있을 때,
+        if graph[r-dx[d]][c-dy[d]] == 1: #후진했는데 벽
+            print(cnt)
+            break
+        else:
+            r,c = r-dx[d],c-dy[d]
